8bbf257770efb2faf1c4f3873ae72f300f614681
syafiq <syafiqardiansyah30@gmail.com>
2021-04-23 23:36:47 +0700

    Tugas 4

---
+++ tugas4.js
@@ -0,0 +1,4 @@
+function prosessSentence(name, age, address, hobby){
+    console.log("Nama saya" + " " + name + "," + " " + "Umur saya" + " " + age + " " + "tahun," + " " + "alamat saya di" + " " + address + "," + " " +"dan saya punya hobby" + " " + hobby);   
+}
+prosessSentence("Agus", 30, "Jln. Malioboro Yogyakarta", "Memancing");

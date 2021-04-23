f3ede0e9bc70800e5e143892195b909b1d4c0aa6
athfal-r <mathfalrizki12@gmail.com>
2021-04-24 01:30:58 +0700

    3

--- function.js
+++ function.js
@@ -5,3 +5,15 @@ function shoutOut() {
 }
 
 console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+
+// Tugas 3
+
+function calculateMultiply(num1, num2) {
+    return num1 * num2 ;
+};
+
+let num1 = 5;
+let num2 = 6;
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30

3b40a36802c112cf1ecbfe0d8358fb59cf0154b2
putrihana <putriwijayanti.mtsalmahysar@gmail.com>
2021-04-24 22:34:49 +0700

    tugas putri

--- function.js
+++ function.js
@@ -11,13 +11,11 @@ console.log(shoutOut());// Menampilkan "Halo Function!" di console
  BUATLAH KODE FUNCTION DISINI
  SOAL 3
 */
-let num1 = 5;
-let num2 = 6;
-
 function calculateMultiply(num1,num2){
     return num1 * num2;
 };
-
+let num1 = 5;
+let num2 = 6;
 console.log(calculateMultiply(num1, num2));
 
 /*
@@ -25,15 +23,16 @@ console.log(calculateMultiply(num1, num2));
  SOAL 4
 */
 
+
+function processSentence(name, age, address, hobby){
+    return 'Nama saya '+name+', umur saya '+age+' tahun, alamat saya di '+address+', dan saya punya hobby yaitu '+hobby;
+}
+
 let name = "Agus";
 let age = 30;
 let address = "Jln. Malioboro, Yogjakarta";
 let hobby = "gaming";
 var fullSentence = processSentence(name, age, address, hobby);
-
-function processSentence(name, age, address, hobby){
-    return 'Nama saya '+name+', umur saya '+age+' tahun, alamat saya di '+address+', dan saya punya hobby yaitu '+hobby;
-}
 
 console.log(fullSentence);
 // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming

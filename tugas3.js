7e87a1a7ac43e947c1cc25af2fc3a905c7a9f926
fariz-bd <budiargof@gmail.com>
2021-04-25 21:39:30 +0700

    tugas fariz

---
+++ tugas3.js
@@ -0,0 +1,6 @@
+function hasilPerkalian(num1, num2){
+    return hasilPerkalian = num1 * num2;
+
+}
+
+console.log(hasilPerkalian(5, 6));
---
+++ tugas4.js
@@ -0,0 +1,5 @@
+function prosesSentence(name, age, addres, hobby){
+    console.log(`Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${addres}, dan saya punya hobby yaitu ${hobby}`)
+}
+
+prosesSentence('agus', 30 , 'Jln. Malioboro, Yogjakarta', 'gaming'); 
